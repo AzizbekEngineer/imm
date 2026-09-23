f1b9225e532d4c1fac303efe21d85ab0ec2de37f
azizbek-frontend-dev <zikriyoyevmurod962@gmail.com>
2026-09-23 22:20:16 +0500

    added

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
-import React, { Suspense } from "react";
+import React, { lazy, Suspense } from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+const App = lazy(() => import("./App.jsx"));
 import "../src/styles/index.scss";
 import Lazy from "./components/leazy/Lazy.jsx";
 
