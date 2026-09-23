a63f06fc0b518c053a260aced827e086a1976f91
azizbek-frontend-dev <zikriyoyevmurod962@gmail.com>
2026-09-23 22:17:40 +0500

    addedd

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-import { Suspense } from "react";
+import React, { Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "../src/styles/index.scss";
